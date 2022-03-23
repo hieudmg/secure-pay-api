d7c55326054621e64f3f96148c5e229ae1a3e542
KhoaZero123 <khoazero123@gmail.com>
2022-03-23 13:34:57 +0700

    Add class CreatePaymentFromInstrumentRequest

--- src/SecurePayApi/Request/CardPayment/CreatePaymentFromInstrumentRequest.php
+++ src/SecurePayApi/Request/CardPayment/CreatePaymentFromInstrumentRequest.php
@@ -6,10 +6,9 @@ use SecurePayApi\Model\Credential;
 use SecurePayApi\Model\Response\CardPayment\PaymentInstrumentObject;
 use SecurePayApi\Request\RestApiRequest;
 
-class CreatePaymentInstrumentRequest extends RestApiRequest
+class CreatePaymentFromInstrumentRequest extends RestApiRequest
 {
     protected string $customerCode;
-    protected array $requestHeaders = [];
 
     public function __construct(bool $isLive, Credential $credential, array $data, string $customerCode)
     {
@@ -24,25 +23,15 @@ class CreatePaymentInstrumentRequest extends RestApiRequest
 
     protected function getEndpoint(): string
     {
-        return $this->buildUrl(parent::getEndpoint(), 'customers', $this->customerCode, 'payment-instruments', 'token');
-    }
-
-    /**
-     * @param array $headers
-     * @return CreatePaymentInstrumentRequest
-     */
-    public function setRequestHeaders(array $headers): CreatePaymentInstrumentRequest
-    {
-        $this->requestHeaders = $headers;
-        return $this;
+        return $this->buildUrl(parent::getEndpoint(), 'customers', $this->customerCode, 'payments');
     }
 
     protected function getRequestHeaders(): array
     {
         $parentHeaders = parent::getRequestHeaders();
         $requestHeaders = array_merge($parentHeaders, [
-            'Content-Type' => 'application/json'
-        ], $this->requestHeaders);
+          'Content-Type' => 'application/json'
+        ]);
         return $requestHeaders;
     }
 }
